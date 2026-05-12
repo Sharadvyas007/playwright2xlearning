58ef6cae3991bbe25508614e4b8b66b4581ffbef
Sharadvyas007 <sharadvyas007@users.noreply.github.com>
2026-05-12 22:50:00 +0530

    Add chapter 02 JavaScript concepts exercises

---
+++ chapter_02_Javascript_Concepts.js/06_identifiers_rules.js
@@ -0,0 +1,27 @@
+var $ = 10;
+var _abc = 12;
+var x = 15;
+
+var abc123 = 34;
+
+// var 123 = 123; // Invalid identifier, cannot start with a number
+
+var Name = "Sharad";
+var name = "Pareek";
+
+//var sharad pareek = "sharad";
+var sharad_pareek = "hello";
+var sharad$pareek = "sharad";
+var sharadpareek123 = "pareek";
+
+
+console.log($);
+console.log(_abc);
+console.log(x);
+console.log(abc123);
+console.log(Name);
+console.log(name);
+console.log(sharad_pareek);
+console.log(sharad$pareek);
+console.log(sharadpareek123);
+
