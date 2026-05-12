0d1a6f64bccf0fff9c32c5057fa96a2b2b590494
Sharadvyas007 <sharadvyas007@users.noreply.github.com>
2026-05-12 23:17:37 +0530

    Merge naming convention examples into 06_identifiers_rules.js and remove separate file

--- chapter_02_Javascript_Concepts.js/06_identifiers_rules.js
+++ chapter_02_Javascript_Concepts.js/06_identifiers_rules.js
@@ -25,3 +25,37 @@ console.log(sharad_pareek);
 console.log(sharad$pareek);
 console.log(sharadpareek123);
 
+// Different naming conventions in JavaScript
+
+// camelCase (variables & functions)
+var firstName = "Sharad";
+var lastName = "Pareek";
+function getUserName() {
+    return firstName + " " + lastName;
+}
+
+// snake_case
+var first_name = "Sharad";
+var last_name = "Pareek";
+
+// PascalCase (classes & constructors)
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+// UPPER_CASE (constants / magic values)
+var MAX_LIMIT = 100;
+var API_KEY = "abc123";
+
+// Mixed examples
+var $count = 0;
+var _privateVar = "hidden";
+var camelCaseWithDigits = "v2";
+
+console.log("camelCase:", firstName + " " + lastName);
+console.log("snake_case:", first_name + " " + last_name);
+console.log("UPPER_CASE:", MAX_LIMIT, API_KEY);
+
